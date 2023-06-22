4135b6515fd0b648f152a4bd49c88c267c94be59
Saidul Islam <saidulislamjihad20@gmail.com>
2023-06-22 12:28:21 +0600

    fatch:Department section design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+//Department List slider Js
+$('.department-items-wrapper').owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: false,
+    nav: true,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6
+        }
+    }
+});
+
 //Doctor List slider Js
 $('.doctor-list-items-wrapper').owlCarousel({
     loop: true,
